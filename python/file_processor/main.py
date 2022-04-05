f6e2681ac9d8c1ad498abe16f4e6564db2bd5707
张东宇 <zhangdongyu6@huawei.com>
2022-04-05 08:06:54 +0000

    [file_processor] [OPT|ADD] 增加重命名时间戳格式照片功能；增加根据正则规则重命名文件功能

--- python/file_processor/main.py
+++ python/file_processor/main.py
@@ -10,99 +10,113 @@ if '__main__' == __name__:
 
 运行模式：
     ****************************** 重命名 ******************************
+
+    1.1 --> 更改文件后缀
+
+    1.2 --> 文件后缀改为小写
+
+    1.3 --> 文件后缀改为大写
+
+    1.4 --> 驼峰文件名转下划线
+
+    1.5 --> 下划线文件名转驼峰
+
+    1.6 --> 根据文件修改时间重命名文件
+
+    1.7 --> 根据文件创建时间重命名文件（仅限Windows）
     
-     1 --> 更改文件后缀
-    
-     2 --> 文件后缀改为小写
-    
-     3 --> 文件后缀改为大写
-     
-     4 --> 驼峰文件名转下划线
-     
-     5 --> 下划线文件名转驼峰
-    
-     6 --> 根据文件修改时间重命名文件
-    
-     7 --> 根据文件创建时间重命名文件（仅限Windows）
-    
-     8 --> 根据照片拍摄时间重命名照片
+    1.8 --> 根据正则规则重命名文件
+
+    1.9 --> 根据照片拍摄时间重命名照片
+
+    1.10 --> 重命名时间戳格式照片（针对微信，美图秀秀等软件保存的照片）
 
     ****************************** 编解码 ******************************
-    
-     9 --> base64方式编码单个文件
 
-    10 --> base64方式解码单个文件
+    2.1 --> base64方式编码单个文件
 
-    11 --> 分割txt文件
+    2.2 --> base64方式解码单个文件
 
-    12 --> 合并txt文件
+    2.3 --> 分割txt文件
+
+    2.4 --> 合并txt文件
     '''
 
     print(msg)
 
     run_mode = input('\n输入运行模式: \n')
 
-    if '1' == run_mode:
+    if '1.1' == run_mode:
         dir = input('请输入文件所在目录: \n')
         ext = input('请输入新后缀（例如：.txt）: \n')
         modify_ext.change_ext(dir, ext)
 
-    elif '2' == run_mode:
+    elif '1.2' == run_mode:
         dir = input('请输入文件所在目录: \n')
         modify_ext.to_lower_case_ext(dir)
 
-    elif '3' == run_mode:
+    elif '1.3' == run_mode:
         dir = input('请输入文件所在目录: \n')
         modify_ext.to_upper_case_ext(dir)
 
-    elif '4' == run_mode:
+    elif '1.4' == run_mode:
         dir = input('请输入文件所在目录: \n')
         rename_file.hump_2_underline_for_dir(dir)
 
-    elif '5' == run_mode:
+    elif '1.5' == run_mode:
         dir = input('请输入文件所在目录: \n')
         rename_file.underline_2_hump_for_dir()
 
-    elif '6' == run_mode:
+    elif '1.6' == run_mode:
         dir = input('请输入文件所在目录: \n')
         format = input('请输入命名格式: (1：默认格式：%Y%m%d_%H%M%S, e.g. 20102029_202550.txt): \n')
         if '1' == format:
             format = '%Y%m%d_%H%M%S'
         rename_file.rename_file_by_modify_time(dir, format)
 
-    elif '7' == run_mode:
+    elif '1.7' == run_mode:
         dir = input('请输入文件所在目录: \n')
         format = input('请输入命名格式: (1：默认格式：%Y%m%d_%H%M%S, e.g. 20102029_202550.txt): \n')
         if '1' == format:
             format = '%Y%m%d_%H%M%S'
         rename_file.rename_file_by_create_time(dir, format)
 
-    elif '8' == run_mode:
-        dir = input('请输入照片所在目录: \n')
-        rename_file.rename_pic(dir)
+    elif '1.8' == run_mode:
+        dir = input('请输入文件所在目录: \n')
+        pattern = input('请输入匹配规则: \n')
+        repl = input('请输入替换规则（用\g<1>反向引用第一个匹配组）: \n')
+        rename_file.rename_file_by_regex(dir, pattern, repl)
 
-    elif '9' == run_mode:
+    elif '1.9' == run_mode:
+        dir = input('请输入照片所在目录: \n')
+        rename_file.rename_pic_by_shooting_time(dir)
+
+    elif '1.10' == run_mode:
+        dir = input('请输入照片所在目录: \n')
+        rename_file.rename_pic_by_timestamp(dir)
+
+    elif '2.1' == run_mode:
         file = input('请输入要编码的文件路径（e.g. D:\\xx.zip）：\n')
         txt = input('请输入编码文件的保存路径（e.g. D:\\xx.txt）：\n')
         encode_file.encode_file(file, txt)
 
-    elif '10' == run_mode:
+    elif '2.2' == run_mode:
         txt = input('请输入编码文件的路径（e.g. D:\\xx.txt）：\n')
         file = input('请输入解码后的文件保存路径（e.g. D:\\xx.zip）：\n')
         encode_file.decode_file(txt, file)
 
-    elif '11' == run_mode:
+    elif '2.3' == run_mode:
         txt = input('请输入要分割的txt文件路径（e.g. D:\\xx.txt）：\n')
         each_file_mb = input('请输入分割大小（单位：MB，e.g. 1）：\n')
         save_dir = input('请输入分割后的txt文件保存目录（e.g. D:\\dir）：\n')
         encode_file.split_txt_file(txt, int(each_file_mb), save_dir)
 
-    elif '12' == run_mode:
+    elif '2.4' == run_mode:
         txt_dir = input('请输入要合并的txt文件目录（e.g. D:\\dir）：\n')
         txt = input('请输入合并后的txt文件保存路径（e.g. D:\\xx.txt）：\n')
         encode_file.merge_txt_file(txt_dir, txt)
 
     else:
-        print('运行模式输入错误!')
+        print('运行模式输入错误，请退出程序，并重新运行！')
 
-    input('输入任意按键退出!')
+    input('输入任意键退出！')
--- python/file_processor/rename_file.py
+++ python/file_processor/rename_file.py
@@ -4,11 +4,12 @@ import time
 import exifread
 from os import walk, rename
 from os.path import basename, join, splitext, getctime, getmtime
+import re
 
 
-def _gen_new_file_name(timeStamp, format):
-    locTime = time.localtime(timeStamp)
-    return time.strftime(format, locTime)
+def _gen_new_file_name(timestamp, format):
+    loc_time = time.localtime(timestamp)
+    return time.strftime(format, loc_time)
 
 
 def _deal_repate_file_name(root, long_name, short_new_name, ext, count):
@@ -35,27 +36,6 @@ def _underline_2_hump(text):
     for i in arr:
         res.append(i[0].upper() + i[1:])
     return ''.join(res)
-
-
-def _dea_repate_pic_name(info, root, pic_name, count):
-    '''
-    Description : 处理多张照片有相同拍摄时间的情况，如果多张照片有相同的拍摄时间，则命名格式为：IMG_20170616_221206.JPG，IMG_20170616_221206_1.JPG，...
-    Modify      : 2020.12.11
-    Input       : IN info：照片属性信息
-                  IN root：照片所在的目录
-                  IN pic_name：照片名字
-                  IN count：拥有相同拍摄时间的照片个数计数器
-    Return      :
-    Caution     :
-    '''
-    new_name = 'IMG_' + info[0:10] + '_' + info[11:11 + 8] + '_' + str(count) + splitext(pic_name)[1]
-    new_name = new_name.replace(':', '')
-    new_name = join(root, new_name)
-    try:
-        rename(pic_name, new_name)
-        print('{} --> {}'.format(basename(pic_name), basename(new_name)))
-    except Exception as e:
-        _dea_repate_pic_name(info, root, pic_name, count + 1)
 
 
 def rename_file_by_modify_time(dir, format):
@@ -116,7 +96,7 @@ def underline_2_hump_for_dir(dir):
             rename(old_name, new_name)
 
 
-def rename_pic(dir):
+def rename_pic_by_shooting_time(dir):
     '''
     Description : 根据照片拍摄时间递归重命名一个目录下所有的照片名字，命名格式为：IMG_20170616_221206.JPG，IMG_20170616_221206_1.JPG，...
     Modify      : 2020.12.11
@@ -126,25 +106,78 @@ def rename_pic(dir):
     '''
     shooting_time_field = 'EXIF DateTimeOriginal'
     for root, dirs, files in walk(dir):
-        for name in files:
+        for file in files:
             # exifread.process_file(fd) 可能抛出异常，所以这里用 try 捕获
             try:
-                pic_name = join(root, name)
-                fd = open(pic_name, 'rb')
-                tags = exifread.process_file(fd)
-                fd.close()
+                base_name, ext = splitext(file)
+                long_name = join(root, file)
+
+                with open(long_name, 'rb') as fd:
+                    tags = exifread.process_file(fd)
+
                 if shooting_time_field in tags:
                     info = str(tags[shooting_time_field])
-                    new_name = 'IMG_' + info[0:10] + '_' + info[11:11 + 8] + splitext(pic_name)[1]
-                    new_name = new_name.replace(':', '')
-                    new_name = join(root, new_name)
+                    short_new_name = 'IMG_' + info[0:10] + '_' + info[11:11 + 8]
+                    short_new_name = short_new_name.replace(':', '')
+                    long_new_name = join(root, short_new_name + ext)
 
                     try:
-                        rename(pic_name, new_name)
-                        print('{} --> {}'.format(basename(pic_name), basename(new_name)))
-                    except Exception as e:
-                        _dea_repate_pic_name(info, root, pic_name, 1)
+                        rename(long_name, long_new_name)
+                        print(f'{basename(long_name)} --> {basename(long_new_name)}')
+                    except Exception:
+                        _deal_repate_file_name(root, long_name, short_new_name, ext, 1)
                 else:
-                    print('pass: {}'.format(basename(pic_name)))
-            except Exception as e:
-                print('pass: {}'.format(name))
+                    print(f'pass: {basename(long_name)}')
+            except Exception:
+                print(f'pass: {file}')
+
+
+def rename_pic_by_timestamp(dir):
+    '''
+    Description : 根据照片名中的时间戳重命名照片（针对微信，美图秀秀等软件保存的照片）
+    Modify      : 2022.4.5
+    Input       : IN dir：目录路径
+    Return      :
+    Caution     :
+    '''
+    for root, dirs, files in walk(dir):
+        for file in files:
+            base_name, ext = splitext(file)
+            long_name = join(root, file)
+
+            # 例如微信照片的命名格式为：mmexport1405241187825.jpg，中间的数字是13位的毫秒级时间戳
+            match_obj = re.search(r"^(\D*)(\d{13})(\D*)$", base_name)
+            if match_obj:
+                timestamp = float(match_obj.group(2)[0:10])  # localtime只能接收秒级的时间戳，也就是10位
+                time.localtime(timestamp)
+                short_new_name = _gen_new_file_name(timestamp, "IMG_%Y%m%d_%H%M%S")
+                long_new_name = join(root, short_new_name + ext)
+
+                try:
+                    rename(long_name, long_new_name)
+                    print(f'{basename(long_name)} --> {basename(long_new_name)}')
+                except Exception:
+                    _deal_repate_file_name(root, long_name, short_new_name, ext, 1)
+
+
+def rename_file_by_regex(dir, pattern, repl):
+    '''
+    Description : 根据正则规则重命名文件
+    Modify      : 2022.4.5
+    Input       : IN dir：目录路径
+    Return      :
+    Caution     :
+    '''
+    for root, dirs, files in walk(dir):
+        for file in files:
+            short_name = file
+            long_name = join(root, file)
+
+            short_new_name = re.sub(pattern, repl, short_name)
+            long_new_name = join(root, short_new_name)
+
+            try:
+                rename(long_name, long_new_name)
+                print(f'{basename(long_name)} --> {basename(long_new_name)}')
+            except Exception:
+                _deal_repate_file_name(root, long_name, splitext(short_new_name)[0], splitext(short_new_name)[1], 1)
