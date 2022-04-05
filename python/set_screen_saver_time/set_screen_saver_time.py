cce7cdd53d6bc08768bed3d98e93526345eaa393
张东宇 <zhangdongyu6@huawei.com>
2022-04-05 08:10:10 +0000

    [set_screen_saver_time] [ADD] 增加设置屏幕超时时间

---
+++ python/set_screen_saver_time/set_screen_saver_time.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+import pyautogui as gui
+
+gui.PAUSE = 1.5
+
+gui.click(16, 1062)
+gui.click(480, 506)
+gui.click(396, 845)
+gui.typewrite(["tab", "6", "0", "enter"])
+gui.click(1895, 18)
